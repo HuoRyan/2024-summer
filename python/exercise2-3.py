8aa02a9181f2fcce78ed6e5ca51de017b27f44bc
HuoRyan <rh3905@nyu.edu>
2024-06-04 10:44:53 +0800

    2-6

--- python/exercise2-3.py
+++ python/exercise2-3.py
@@ -2,11 +2,14 @@ year = int(input("Year:"))
 if year % 4 != 0:
     print("Not leap year")
 else:
-    if year % 100 == 0 and year % 400 != 0:
-        print("Not leap year")
+    if year % 100 == 0 :
+        if year % 400 != 0:
+            print("Not leap year")
+        else:
+            print("Leap year")
     else:
         print("Leap year")
 if (year-1950) % 4 == 0 and year > 1950:
     print("World Cup year")
-elif (year-1960) % 4 == 0 and year > 1960:
+if (year-1960) % 4 == 0 and year > 1960:
     print("Euro Cup year")
